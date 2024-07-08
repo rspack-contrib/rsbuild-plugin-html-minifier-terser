edb4e4ea9fb096f251dd144f1de06eab23633f45
neverland <chenjiahan.jait@bytedance.com>
2024-07-08 19:10:41 +0800

    test: add e2e case

--- test/basic/index.test.ts
+++ test/basic/index.test.ts
@@ -1,31 +1,23 @@
-import { dirname } from 'node:path';
+import { readFileSync } from 'node:fs';
+import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { expect, test } from '@playwright/test';
 import { createRsbuild } from '@rsbuild/core';
 import { pluginHtmlMinifierTerser } from '../../src';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+const fixtures = __dirname;
 
-test('should render page as expected', async ({ page }) => {
+test('should minify template as expected', async ({ page }) => {
 	const rsbuild = await createRsbuild({
 		cwd: __dirname,
 		rsbuildConfig: {
-			plugins: [pluginHtmlMinifierTerser()],
-		},
-	});
-
-	const { server, urls } = await rsbuild.startDevServer();
-
-	await page.goto(urls[0]);
-	expect(await page.evaluate('window.test')).toBe(1);
-
-	await server.close();
-});
-
-test('should build succeed', async ({ page }) => {
-	const rsbuild = await createRsbuild({
-		cwd: __dirname,
-		rsbuildConfig: {
+			html: {
+				template: './src/index.html',
+			},
+			performance: {
+				removeConsole: ['log', 'warn'],
+			},
 			plugins: [pluginHtmlMinifierTerser()],
 		},
 	});
@@ -34,7 +26,29 @@ test('should build succeed', async ({ page }) => {
 	const { server, urls } = await rsbuild.preview();
 
 	await page.goto(urls[0]);
-	expect(await page.evaluate('window.test')).toBe(1);
+
+	const test = page.locator('#test');
+
+	await expect(test).toHaveCSS('text-align', 'center');
+	await expect(test).toHaveCSS('font-size', '146px');
+	await expect(test).toHaveText('Hello Rsbuild!');
+	await expect(page.evaluate('window.b')).resolves.toBe(2);
+
+	const htmlFile = join(__dirname, 'dist/index.html');
+	const content = readFileSync(htmlFile, 'utf-8');
+
+	expect(
+		content.includes('.test{font-size:146px;background-color:green}'),
+	).toBeTruthy();
+	expect(
+		content.includes('#a{text-align:center;line-height:1.5;font-size:1.5rem}'),
+	).toBeTruthy();
+	expect(content.includes('window.a=1,window.b=2')).toBeTruthy();
+	expect(content.includes('console.info(111111)')).toBeTruthy();
+	expect(content.includes('console.warn(111111)')).toBeFalsy();
+
+	// keep html comments
+	expect(content.includes('<!-- HTML COMMENT-->')).toBeTruthy();
 
 	await server.close();
 });
