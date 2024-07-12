13b26fe6d631a6770bbdaf215c985a906e184847
fi3ework <fi3ework@gmail.com>
2024-07-12 10:25:03 +0800

    feat: support customize options (#1)

---
+++ test/config/src/index.ts
@@ -0,0 +1,7 @@
+import './App.css';
+const testEl = document.createElement('div');
+testEl.id = 'test';
+testEl.className = 'test';
+testEl.innerHTML = 'Hello Rsbuild!';
+
+document.body.appendChild(testEl);
